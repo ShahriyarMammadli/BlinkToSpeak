a26abca0eec09b746d1a028ab01bfe5b0d232cc2
Shahriyar Mammadli <smammadli@pasha-holding.ph>
2021-02-10 15:59:15 +0400

    Decoder and camera functionalities added and improved

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,2 +1,53 @@
 # Shahriyar Mammadli
 # Import required libraries
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import imutils
+import time
+import dlib
+import cv2
+
+# The function obtains the landmarks of an eye and calculates the...
+# ...eye aspect ratio of it.
+def EAR(eye):
+    # Calculate euclidean distance between upper and lower landmark...
+    # ...couples.
+    p2p6 = dist.euclidean(eye[1], eye[5])
+    p3p5 = dist.euclidean(eye[2], eye[4])
+    # Calculate euclidean distance between side landmarks.
+    p1p4 = dist.euclidean(eye[0], eye[3])
+    # Calculate ear aspect ratio
+    ear = (p2p6 + p3p5) / (2.0 * p1p4)
+    # return the eye aspect ratio
+    return ear
+
+# The function draws necessary details on the frame.
+def drawOnStream(frame, rect, index, shape, leftEye, rightEye):
+    # Draw a rectangle around the face.
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Write the face number next to the face.
+    cv2.putText(frame, "Face #{}".format(index + 1), (x - 10, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # Draw the all face landmarks
+    for (x, y) in shape:
+        cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+    # Draw the eyes using the corresponding landmarks.
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+# This function draws output of the process on the stream.
+def drawResults(frame, totalLeftEyeBlinks, totalRightEyeBlinks, totalBothEyeBlinks, averageEAR):
+    cv2.putText(frame, "Left Eye Blinks: {}".format(totalLeftEyeBlinks), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, "Right Eye Blinks: {}".format(totalRightEyeBlinks), (10, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, "Both Eyes Blinks: {}".format(totalBothEyeBlinks), (10, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, "Average EAR: {:.5f}".format(averageEAR), (290, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
--- main.py
+++ main.py
@@ -1,148 +1,120 @@
 # Shahriyar Mammadli
-# Import required libraries
+# Import required libraries.
 import helperFunctions as hf
-
-# import the necessary packages
-from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
-from imutils.video import VideoStream
-from imutils import face_utils
 import imutils
-import time
-import dlib
+from imutils import face_utils
 import cv2
+import dlib
+import morseDecoder as md
 
-# Define the face landmark path
+# Set the parameters.
+# Define the face landmark path.
 landmarkPath = "shape_predictor_68_face_landmarks.dat"
-
-def eye_aspect_ratio(eye):
-    # compute the euclidean distances between the two sets of
-    # vertical eye landmarks (x, y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
-
-    # compute the euclidean distance between the horizontal
-    # eye landmark (x, y)-coordinates
-    C = dist.euclidean(eye[0], eye[3])
-
-    # compute the eye aspect ratio
-    ear = (A + B) / (2.0 * C)
-
-    # return the eye aspect ratio
-    return ear
-
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.21
-RATIO_THRESH = 0.0017
-EYE_AR_CONSEC_FRAMES = 3
-
-# initialize the frame counters and the total number of blinks
-COUNTER = 0
-TOTAL = 0
-
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
+# Threshold value for EAR.
+EARThreshold = 0.225
+#  The threshold for number of consecutive frames to wait before...
+#  ...validating eye blink.
+conFrames = 3
+# Counter of current number of frames that eye-blink has been continuing.
+curLeftEyeBlinkFrames = 0
+curRightEyeBlinkFrames = 0
+curBothEyeBlinkFrames = 0
+# Total number of eye blinks.
+totalLeftEyeBlinks = 0
+totalRightEyeBlinks = 0
+totalBothEyeBlinks = 0
+# Set the frame width.
+frameWidth = 450
+# The face detector of dlib is used to detect faces in a frame.
+faceDetector = dlib.get_frontal_face_detector()
+# The landmark detector initialization.
 predictor = dlib.shape_predictor(landmarkPath)
+# Indices of the landmarks of each eye.
+(leftStart, leftEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rightStart, rightEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+# Initialization of video stream.
+videoStream = imutils.video.VideoStream(src=0).start()
 
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+# Message content will be hold in this variable
+message = ""
 
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = FileVideoStream(landmarkPath).start()
-fileStream = True
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
-fileStream = False
-time.sleep(1.0)
-
-# loop over frames from the video stream
+# Run the system while 'q' is not pressed.
 while True:
-    # if this is a file video stream, then we need to check if
-    # there any more frames left in the buffer to process
-    if fileStream and not vs.more():
-        break
-
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale
-    # channels)
-    frame = vs.read()
-    frame = imutils.resize(frame, width=450)
+    # Obtain the frames from the camera steam.
+    frame = videoStream.read()
+    # Resize the image.
+    frame = imutils.resize(frame, width=frameWidth)
+    # Convert the image to grayscale before detecting landmarks.
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect the faces in the frame.
+    faces = faceDetector(gray, 0)
 
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
-
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
+    # Iterate over the faces.
+    for (index, rect) in enumerate(faces):
+        # Get the landmarks of a face
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-
-        # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        ear_ratio = ear / w
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        # Select the landmarks of left and right eyes.
+        leftEye = shape[leftStart:leftEnd]
+        rightEye = shape[rightStart:rightEnd]
+        leftEAR = hf.EAR(leftEye)
+        rightEAR = hf.EAR(rightEye)
+        # average the eye aspect ratio together for both eyes.
+        averageEAR = (leftEAR + rightEAR) / 2.0
+        # (x, y, w, h) = face_utils.rect_to_bb(rect)
+        # ear_ratio = ear / w
+        # Draw the details on the frame.
+        hf.drawOnStream(frame, rect, index, shape, leftEye, rightEye)
 
         # check to see if the eye aspect ratio is below the blink
         # threshold, and if so, increment the blink frame counter
-        if ear_ratio < RATIO_THRESH:
-            # if ear < EYE_AR_THRESH:
-            COUNTER += 1
-        # print("========EAR=========")
-        # print(ear)
-        # print("====================")
-        # print(w)
-        # print("====================")
-        # print(ear_ratio)
-
+        if leftEAR < EARThreshold and rightEAR < EARThreshold and abs(leftEAR - rightEAR) < 0.015:
+            curBothEyeBlinkFrames += 1
+            curLeftEyeBlinkFrames += 1
+            curRightEyeBlinkFrames += 1
+        elif leftEAR < EARThreshold and rightEAR - leftEAR > 0.015:
+            curLeftEyeBlinkFrames += 1
+        elif rightEAR < EARThreshold and leftEAR - rightEAR > 0.015:
+            curRightEyeBlinkFrames += 1
         # otherwise, the eye aspect ratio is not below the blink
         # threshold:
         else:
             # if the eyes were closed for a sufficient number of
             # then increment the total number of blinks
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                TOTAL += 1
+            if curLeftEyeBlinkFrames >= conFrames:
+                totalLeftEyeBlinks += 1
+                print(leftEAR)
+                print(rightEAR)
+            if curRightEyeBlinkFrames >= conFrames:
+                totalRightEyeBlinks += 1
+                print(leftEAR)
+                print(rightEAR)
+            if curLeftEyeBlinkFrames >= conFrames and curRightEyeBlinkFrames >= conFrames:
+                totalBothEyeBlinks += 1
+            # Build message content
+            if curLeftEyeBlinkFrames >= conFrames > curRightEyeBlinkFrames:
+                message += '.'
+            elif curRightEyeBlinkFrames >= conFrames > curLeftEyeBlinkFrames:
+                message += '-'
+            elif curLeftEyeBlinkFrames >= conFrames and curRightEyeBlinkFrames >= conFrames:
+                message += ' '
 
+            print(message)
+            decryptedMessage = md.decrypt(message)
+            print(decryptedMessage)
             # reset the eye frame counter
-            COUNTER = 0
-
-        # draw the total number of blinks on the frame along with
-        # the computed eye aspect ratio for the frame
-        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.5f}".format(ear), (300, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-    # show the frame
-    cv2.imshow("Frame", frame)
+            curLeftEyeBlinkFrames = 0
+            curRightEyeBlinkFrames = 0
+            curBothEyeBlinkFrames = 0
+        # Draw results.
+        hf.drawResults(frame, totalLeftEyeBlinks, totalRightEyeBlinks, totalBothEyeBlinks, averageEAR)
+    cv2.imshow("BlinkToSpeak", frame)
     key = cv2.waitKey(1) & 0xFF
 
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
 
-# do a bit of cleanup
+# Shut off the stream.
 cv2.destroyAllWindows()
-vs.stop()
+videoStream.stop()
